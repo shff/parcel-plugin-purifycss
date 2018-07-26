5d37cdacc2dcdc385c6a31aab3ba499e04ed4539
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2018-07-26 20:03:06 -0300

    Add svelte support

--- purifyCss.js
+++ purifyCss.js
@@ -3,7 +3,7 @@ const purify = require("purifycss")
 
 class PurifiedCssAsset extends CSSAsset {
   async load() {
-    const extensions = [ "html", "js", "jsx", "vue" ];
+    const extensions = [ "html", "js", "jsx", "vue", "svelte" ];
     const content = extensions.map(ext => `${ this.options.rootDir }/**/*.${ ext }`);
     const source = await super.load();
     return await new Promise(r => purify(content, source, r));
