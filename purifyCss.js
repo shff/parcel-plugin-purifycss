4bc5b6383ad9f0e53dcf68119d15b65d03487512
Silvio Henrique Ferreira <shferreira@me.com>
2018-08-07 20:31:58 -0300

    Don't run on dev builds

--- purifyCss.js
+++ purifyCss.js
@@ -3,6 +3,10 @@ const purify = require("purifycss")
 
 class PurifiedCssAsset extends CSSAsset {
   async load() {
+    if (process.env.NODE_ENV !== "production") {
+      return await super.load();
+    }
+
     const extensions = [ "html", "js", "jsx", "vue", "svelte" ];
     const content = extensions.map(ext => `${ this.options.rootDir }/**/*.${ ext }`);
     const source = await super.load();
